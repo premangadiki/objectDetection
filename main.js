bc10fa9482e85c79ca8becd11cc5bf5f6cc0b759
premangadiki <87991780+premangadiki@users.noreply.github.com>
2022-01-31 19:17:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 status ="";
+objects =[];
 
 function preload(){
     img = loadImage('dog_cat.jpg');
@@ -27,20 +28,24 @@ function gotresults(error , results){
 
 else{
     console.log(results);
+    objects = results;
 }
 }
 
 function draw(){
     image(img , 0 , 0 , 640 , 420);
-    fill("#FF0000");
-    text("dog" , 45 , 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30 , 60 , 450 , 350);
+    
+    if( status != ""){
+        for( i = 0 ; i < objects.length ; i++ ){
 
-    fill("#FF0000");
-    text("cat" , 320 , 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300 , 90 , 270 , 320);
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" + objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x ,objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
 }
